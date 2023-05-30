4db2ad898173b9a1788ffe7d15090b290c08d71c
Sophie Abraham <sophie.abraham@posteo.de>
2023-05-30 20:14:37 +0200

    + added vite-react boilerplate

---
+++ src/js/App.jsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react';
+import '../css/App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='container mx-auto'>
+        <h1 className='text-4xl text-center py-9'>portfolio.</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
