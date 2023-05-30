037ee0ec645d9c6eaa19f41eaf657bbd842d6aa6
Sophie Abraham <sophie.abraham@posteo.de>
2023-05-30 22:14:53 +0200

    + added version 1.0

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,13 +1,52 @@
-//import { useState } from 'react';
+import { RxLinkedinLogo, RxGithubLogo } from 'react-icons/rx';
+import { BiCopyright } from 'react-icons/bi';
+
 import '../css/App.css';
+
 
 function App() {
 
   return (
-    <div className="App">
-      <div className='container mx-auto'>
-        <h1 className='text-4xl text-center py-9'>portfolio.</h1>
+    <div className="App bg-darkblue h-screen">
+
+      <div className='container w-fit h-1/2 mx-auto flex flex-col justify-center items-start tracking-widest'>
+
+        <h1 className='text-5xl text-pastellblue py-9'>portfolio.</h1>
+
+        <h2 className='text-beige text-4xl mt-6'>sophie abraham.</h2>
+        <h3 className='text-beige text-3xl mt-6'>junior frontend developer.</h3>
+
+        <div className='flex justify-center items-start text-pastellgreen text-5xl mt-24'>
+          <a
+            href="https://www.linkedin.com/in/sophie-abraham/"
+            target="_blank"
+          >
+            <RxLinkedinLogo className='hover:scale-110 transition duration-300 ease-in-out' />
+          </a>
+
+          <a
+            href="https://github.com/sophieAbraham"
+            target="_blank"
+          >
+            <RxGithubLogo className='ml-5 hover:scale-110 transition duration-300 ease-in-out' />
+          </a>
+        </div>
+
       </div>
+
+      <footer className='fixed bottom-5 right-0 flex justify-center items-center w-full'>
+        <ul className="flex justify-around items-center w-full md:w-1/2 tracking-widest text-pastellblue md:text-2xl text-base">
+
+          <li>sophie abraham</li>
+
+          <li className="flex justify-center items-center">
+            <BiCopyright className="text-3xl mr-1" />
+            Copyright 2023
+          </li>
+
+        </ul>
+      </footer>
+
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily:{
+        MontserratAlt: 'Montserrat Alternates, sans-serif'
+      },
+
+      colors: {
+        'darkblue': '#11324D',
+        'pastellblue': '#6B7AA1',
+        'pastellgreen': '#C1CFC0',
+        'beige': '#E7E0C9'
+      },
+    },
   },
   plugins: [],
 }
